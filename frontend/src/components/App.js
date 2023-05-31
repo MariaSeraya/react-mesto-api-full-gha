015e9de18ebf1a72e799b23ce50aae659b0671b2
Maria Seraya <seraya.maria.a@gmail.com>
2023-05-31 21:45:34 +0300

    Fix

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -90,8 +90,6 @@ function App() {
         }
       })
       .catch((error) => {
-        setIsLoggedIn(false);
-        setIsInfoTooltipPopupOpen(true);
         console.log(error);
       })
   }
